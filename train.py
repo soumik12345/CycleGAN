40ea3f5b35b47acaa017b7578ca7063c3b01bf7d
soumik12345 <19soumik.rakshit96@gmail.com>
2020-07-02 19:18:41 +0530

    Added Train Step

--- train.py
+++ train.py
@@ -90,10 +90,10 @@ class Trainer:
         else:
             return self.mse_loss(prediction, tf.zeros_like(prediction))
 
-    def calc_cycle_loss(self, reconstructed_images, real_images):
+    def calculate_cycle_loss(self, reconstructed_images, real_images):
         return self.mae_loss(reconstructed_images, real_images)
 
-    def calc_identity_loss(self, identity_images, real_images):
+    def calculate_identity_loss(self, identity_images, real_images):
         return self.mae_loss(identity_images, real_images)
 
     def get_dataset(self):
@@ -125,3 +125,62 @@ class Trainer:
         else:
             print("Initializing from scratch.")
         return checkpoint, checkpoint_manager
+
+    @tf.function
+    def train_generator(self, images_a, images_b):
+        real_a = images_a
+        real_b = images_b
+
+        with tf.GradientTape() as tape:
+            # Cycle A -> B -> A
+            fake_a2b = self.generator_a2b(real_a, training=True)
+            recon_b2a = self.generator_b2a(fake_a2b, training=True)
+            # Cycle B -> A -> B
+            fake_b2a = self.generator_b2a(real_b, training=True)
+            recon_a2b = self.generator_a2b(fake_b2a, training=True)
+
+            # Use real B to generate B should be identical
+            identity_a2b = self.generator_a2b(real_b, training=True)
+            identity_b2a = self.generator_b2a(real_a, training=True)
+            loss_identity_a2b = self.calculate_identity_loss(identity_a2b, real_b)
+            loss_identity_b2a = self.calculate_identity_loss(identity_b2a, real_a)
+
+            # Generator A2B tries to trick Discriminator B that the generated image is B
+            loss_gan_gen_a2b = self.calculate_gan_loss(
+                self.discriminator_b(fake_a2b, training=True), True
+            )
+            # Generator B2A tries to trick Discriminator A that the generated image is A
+            loss_gan_gen_b2a = self.calculate_gan_loss(
+                self.discriminator_a(fake_b2a, training=True), True
+            )
+            loss_cycle_a2b2a = self.calculate_cycle_loss(recon_b2a, real_a)
+            loss_cycle_b2a2b = self.calculate_cycle_loss(recon_a2b, real_b)
+
+            # Total generator loss
+            loss_gen_total = loss_gan_gen_a2b + loss_gan_gen_b2a \
+                             + (loss_cycle_a2b2a + loss_cycle_b2a2b) * self.configs['lambda_cycle'] \
+                             + (loss_identity_a2b + loss_identity_b2a) * self.configs['lambda_id']
+
+        trainable_variables = self.generator_a2b.trainable_variables + self.generator_b2a.trainable_variables
+        gradient_gen = tape.gradient(loss_gen_total, trainable_variables)
+        self.generator_optimizer.apply_gradients(zip(gradient_gen, trainable_variables))
+
+        # Metrics
+        self.loss_gen_a2b_metrics(loss_gan_gen_a2b)
+        self.loss_gen_b2a_metrics(loss_gan_gen_b2a)
+        self.loss_id_b2a_metrics(loss_identity_b2a)
+        self.loss_id_a2b_metrics(loss_identity_a2b)
+        self.loss_cycle_a2b2a_metrics(loss_cycle_a2b2a)
+        self.loss_cycle_b2a2b_metrics(loss_cycle_b2a2b)
+        self.loss_gen_total_metrics(loss_gen_total)
+
+        loss_dict = {
+            'loss_gen_a2b': loss_gan_gen_a2b,
+            'loss_gen_b2a': loss_gan_gen_b2a,
+            'loss_id_a2b': loss_identity_a2b,
+            'loss_id_b2a': loss_identity_b2a,
+            'loss_cycle_a2b2a': loss_cycle_a2b2a,
+            'loss_cycle_b2a2b': loss_cycle_b2a2b,
+            'loss_gen_total': loss_gen_total,
+        }
+        return fake_a2b, fake_b2a, loss_dict
