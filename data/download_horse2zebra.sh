f9750f8672a1395d13bfba0f3e2ba87e4790b68b
soumik12345 <19soumik.rakshit96@gmail.com>
2020-07-09 21:17:43 +0530

    Added Dataset Download Script

---
+++ data/download_horse2zebra.sh
@@ -0,0 +1,7 @@
+wget https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/horse2zebra.zip
+unzip horse2zebra.zip
+rm horse2zebra.zip
+cd horse2zebra/trainA
+wget https://www.dropbox.com/s/hhsdx4ch5wxt6kq/horses_extra.zip
+unzip horses_extra.zip
+rm horses_extra.zip
