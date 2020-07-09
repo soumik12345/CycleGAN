32f13ba143f98d803cb8ad6221fe4b91abf1c2ca
soumik12345 <19soumik.rakshit96@gmail.com>
2020-07-10 00:06:43 +0530

    Updated Horse2Zebra download script

--- data/download_horse2zebra.sh
+++ data/download_horse2zebra.sh
@@ -5,3 +5,5 @@ cd horse2zebra/trainA
 wget https://www.dropbox.com/s/hhsdx4ch5wxt6kq/horses_extra.zip
 unzip horses_extra.zip
 rm horses_extra.zip
+mv horse2zebra/trainA/horses_extra_54/* horse2zebra/trainA/
+rm -r horse2zebra/trainA/horses_extra_54/
